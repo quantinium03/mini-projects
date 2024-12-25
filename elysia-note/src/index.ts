bd60bd400eafa643257cf67aaf6f3a258e6b2ab4
quantinium03 <vaibhavvx331@gmail.com>
2024-12-26 03:11:34 +0530

    fixed?

---
+++ elysia-note/src/index.ts
@@ -0,0 +1,18 @@
+import { Elysia } from 'elysia'
+import { swagger } from '@elysiajs/swagger'
+import { opentelemetry } from '@elysiajs/opentelemetry'
+
+import { note } from './note'
+import { user } from './user'
+
+const app = new Elysia()
+    .use(opentelemetry())
+    .use(swagger())
+    .onError(({ error, code }) => {
+        if (code === 'NOT_FOUND') return 'Not Found :('
+
+        console.error(error)
+    })
+    .use(user)
+    .use(note)
+    .listen(3000)
